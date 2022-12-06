08a97754cf22e376ea21e0bb63319616cbaecb7c
Alexander Tokarev <atokarev22@fed.idserve.net>
2022-12-06 18:04:59 +0100

    - Added Firebase dependency and FB configuration

--- CoffeeBreak/CoffeeBreak/CoffeeBreakApp.swift
+++ CoffeeBreak/CoffeeBreak/CoffeeBreakApp.swift
@@ -6,12 +6,27 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct CoffeeBreakApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+// MARK: - UIKit AppDelegate, required for Firebase
+
+fileprivate class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+  ) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
 }
