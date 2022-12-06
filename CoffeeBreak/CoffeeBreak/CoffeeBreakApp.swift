515cb80df70d50e21a2a77615e28178105c19c72
Alexander Tokarev <atokarev22@fed.idserve.net>
2022-12-06 17:30:55 +0100

    - Empty project commit

---
+++ CoffeeBreak/CoffeeBreak/CoffeeBreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeBreakApp.swift
+//  CoffeeBreak
+//
+//  Created by Alexander Tokarev on 06/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeBreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
