5bd5ffb7be93929700f5af091725421913ce62d2
Alexander Tokarev <atokarev22@fed.idserve.net>
2022-12-14 17:20:07 +0100

    - Extend DiscussionTopic with more cases

--- CoffeeBreak/CoffeeBreak/Model/DiscussionTopic.swift
+++ CoffeeBreak/CoffeeBreak/Model/DiscussionTopic.swift
@@ -9,12 +9,36 @@ import Foundation
 
 enum DiscussionTopic: String, Identifiable, Hashable, CaseIterable {
     var id: String { self.rawValue }
-    
+    // Coding
     case coding
-    case food
+    case swift
+    case swiftUi
+    case uiKit
+    // Design
     case design
+    case uiDesign
+    case uxDesign
+    // Gaming
+    case gaming
+    case gameDevelopment
+    // Sports
+    case sports
+    case football
+    case skiing
+    case hiking
+    // Etc
+    case food
 }
 
 extension DiscussionTopic {
-    var title: String { self.rawValue.uppercased() }
+    var title: String {
+        switch self {
+        case .swiftUi: return "SwiftUI"
+        case .uiKit: return "UIKit"
+        case .uiDesign: return "UI Design"
+        case .uxDesign: return "UX Design"
+        case .gameDevelopment: return "Game Development"
+        default: return self.rawValue.uppercased()
+        }
+    }
 }
